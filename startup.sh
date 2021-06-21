4c46cf02400a1ab3db746b54f9fe2611b0d0462f
parallacks <34989202+parallacks@users.noreply.github.com>
2021-06-20 21:27:33 -0600

    Couple of changes, made startup script check for changes in repo for potential update automation

--- startup.sh
+++ startup.sh
@@ -1,1 +1,3 @@
+git -C /home/dietpi/Framify/ reset HEAD --hard
+git -C /home/dietpi/Framify/ pull
 forever start /home/dietpi/Framify/index.js
--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 cd /home/dietpi/Frameify
-git reset --hard HEAD
+git -C /home/dietpi/Framify/ reset --hard HEAD
 output = $(git pull | grep "Already up to date")
 if [[ -z $output ]]
 then
-    cp index.html /var/www/index.html
+    cp /home/dietpi/Framify/index.html /var/www/index.html
     npm install
     forever stopall
     forever start /home/dietpi/Frameify/index.js
